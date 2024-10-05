612867e90608dbef6c71fc2e2ea5af2408e6a165
saurabhack <kamanesaurabh68@gmail.com>
2024-10-05 13:58:36 +0530

    something add

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['animate.css']
     }
   }
-}
-
+})
